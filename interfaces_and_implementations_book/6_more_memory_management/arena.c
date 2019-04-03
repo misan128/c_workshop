e1eafdcb08484619f45f9e394e28a87e433bd598
Sandoval Michael Stiven <mssandoval@ikerlan.es>
2019-04-03 15:45:59 +0200

    adds chapter 6 and 7

--- interfaces_and_implementations_book/6_more_memory_management/arena.c
+++ interfaces_and_implementations_book/6_more_memory_management/arena.c
@@ -10,27 +10,168 @@
 const Except_T Arena_NewFailed = { "Arena Creation Failed" };
 const Except_T Arena_Failed = { "Arena Allocation Failed" };
 
-// <macros>
+//////////////
+// <macros> //
+//////////////
+#define THRESHOLD 10
 
-// <types>
+/////////////
+// <types> //
+/////////////
+
+/**
+ * The size of the union give the minimun alignment on the host machine. Its fields
+ * are those thar are most likely to have the strictest alignment requirements, and
+ * it is used to round nbytes.
+ */
+union align {
+	int i;
+	long l;
+	long *lp;
+	void *p;
+	void (*fp)(void);
+	float f;
+	double d;
+	long double ld;
+};
+
+/**
+ * Ensures that new arena->avail is set to a properly aligned addres for the
+ * first allocationn of this chunk.
+ */
+union header {
+	struct T b;
+	union align a;
+};
+
+/**
+ * Arena struct
+ */
 struct T {
 	T prev; // Points to the head of the chunk
 	char *avail; // Points to the chunk's first free location
 	char *limit; // The space between avail and limit is available for allocation
-}
+};
 
-// <data>
 
-// <functions>
+////////////
+// <data> //
+////////////
+
+static T freechunks;
+static int nfree;
+
+
+/////////////////
+// <functions> //
+/////////////////
 
 /**
- * @param
+ * Allocates a space chunk in an already initialized arena structure. Rounds the requested
+ * amount up to the proper alignment boundary, increments the avail pointer by the amount
+ * of the rounded request, and returns the previous value.
+ * 
+ * @param  {T} 				arena   Arena structure
+ * @param  {long} 			nbytes  Number of bytes to allocate in arena
+ * @param  {const char *} 	file   	Execution file name fot exception
+ * @param  {int} 			line   	Execution line number for exception
+ * @return        			Pointer to allocated memory space
  */
-void Arena_dispose(int o) {
+void *Arena_alloc(T arena, long nbytes, const char *file, int line){
+	assert(arena);
+	assert(nbytes > 0);
+
+	// <round nbytes up to an alignment boundary>
+	nbytes = ((nbytes + sizeof(union align) - 1) / (sizeof(union align))) * (sizeof(union align));
+	
+	while(nbytes > arena->limit - arena->avail){
+		// <get a new chunk>
+		T ptr;
+		char *limit;
+
+		// <ptr <- a new chunk>
+		if ((ptr = freechunks) != NULL){
+			freechunks = freechunks->prev;
+			nfree--;
+			limit = ptr->limit;
+		} else {
+			long m = sizeof(union header) + nbytes + 10*1024;
+			ptr = malloc(m);
+			if (ptr == NULL){
+				// <raise Arena_Failed>
+				if (file == NULL){ RAISE(Arena_Failed); }
+				else { Except_raise(&Arena_Failed, file, line); }
+
+			}
+			limit = (char *)ptr + m;
+		}
+		
+		*ptr = *arena;
+		arena->avail = (char *)((union header *)ptr + 1);
+		arena->limit = limit;
+		arena->prev = ptr;
+	}
+
+	arena->avail += nbytes;
+	return arena->avail - nbytes;
+}
+
+/**
+ * Allocates a contiguous number of bytes. Uses Arena_alloc
+ * @param  {T} 				arena   Arena structure
+ * @param  {long}			count	Number of blocks of nbytes size to allocate
+ * @param  {long} 			nbytes  Number of bytes of each count block
+ * @param  {const char *} 	file   	Execution file name fot exception
+ * @param  {int} 			line   	Execution line number for exception
+ * @return        			Pointer to allocated memory space
+ */
+void *Arena_calloc(T arena, long count, long nbytes, const char *file, int line){
+	void *ptr;
+
+	assert(count > 0);
+	ptr = Arena_alloc(arena, count * nbytes, file, line);
+	memset(ptr, '\0', count * nbytes);
+	return ptr;
+}
+
+/**
+ * Deallocates the chunks in the arena; then frees the arena structure itself and clears
+ * the pointer to arena
+ * 
+ * @param {T} ap Arena structure pointer
+ */
+void Arena_dispose(T *ap) {
 	assert(ap && *ap);
 	Arena_free(*ap);
 	free(*ap);
 	*ap = NULL;
+}
+
+/**
+ * Frees teh allocated chunks space of an arena. If the number of free arenas is lower than
+ * THRESHOLD the pointer to the begining of the memory block, otherwise it'll calls free()
+ * 
+ * @param {T} arena Arena structure to deallocate
+ */
+void Arena_free(T arena) {
+	assert(arena);
+	while (arena->prev) {
+		struct T tmp = *arena->prev;
+
+		// <free the chunk described by arena>
+		if (nfree < THRESHOLD) {
+			arena->prev->prev = freechunks;
+			freechunks = arena->prev;
+			nfree++;
+			freechunks->limit = arena->limit;
+		} else {
+			free(arena->prev);
+		}
+		
+		*arena = tmp;
+	}
+	assert(arena->limit == NULL);
+	assert(arena->avail == NULL);
 }
 
 
@@ -38,7 +179,7 @@ void Arena_dispose(int o) {
  * Allocates and returns an arena structure with it's fields set to null pointers
  * which denotes an empty arena
  * 
- * @return 		New allocated arena structure
+ * @return New allocated arena structure
  */
 T Arena_new(void){
 	T arena = malloc(sizeof(*arena));
---
+++ interfaces_and_implementations_book/7_lists/list.c
@@ -0,0 +1,188 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include "assert.h"
+#include "mem.h"
+#include "list.h"
+
+#define T List_T
+
+// <funtions>
+
+/**
+ * Appends one list to another. Assigns to the last rest field in list
+ * @param  {T} list List to append
+ * @param  {T} tail List to be appended
+ * @return     List with tail appended to list
+ */
+T List_append (T list, T tail){
+	T *p = &list;
+
+	while(*p)
+		p = &(*p)->rest;
+	*p = tail;
+
+	return list;
+}
+
+/**
+ * Makes and returns a copy of list
+ * @param  {T} list   List to be copied
+ * @return     Copy of list
+ */
+T List_copy (T list) {
+	T head, *p = &head;
+
+	for ( ; list; list = list->rest) {
+		NEW(*p);
+		(*p)->first = list->first;
+		p = &(*p)->rest;
+	}
+	*p = NULL;
+
+	return head;
+}
+
+/**
+ * If list is nonnull deallocates all of the nodes on list and sets ir to the null
+ * pointer
+ * 
+ * @param {T} list   Pointer to a List
+ */
+void List_free (T *list) {
+	T next;
+
+	assert(list);
+	for ( ; *list; *list = next) {
+		next = (*list)->rest;
+		FREE(*list);
+	}
+}
+
+/**
+ * Creates and returns a list. It's called with N nonnull pointers followed by one
+ * null pointer, and it creates a list with N nodes whose first fields hold the N
+ * nonnull pointers and whose Nth rest field is null.
+ * Any pointer different to void passed in the variable part of its argument needs
+ * to be casted.
+ *
+ * @param {void *} x   Pointer to element to include in list
+ * @param {va_arg} ... Variable argument pointers to be included in list
+ */
+T List_list (void *x, ...) {
+	va_list ap;
+	T list, *p = &list;
+
+	va_start(ap, x);
+	for ( ; x; x = va_arg(ap, void *)) {
+		NEW(*p);
+		(*p)->first = x;
+		p = &(*p)->rest;
+	}
+	*p = NULL;
+	va_end(ap);
+	return list;
+}
+
+/**
+ * Returns the number of nodes in list
+ * @param  {T} list   List to count nodes
+ * @return     Number of nodes in list
+ */
+int List_length(T list) {
+	int n;
+
+	for (n = 0; list; list = list->rest)
+		n++
+	return n;
+}
+
+/**
+ * Calls the function pointed to by apply for every node in list. Clients can pass
+ * an application-specific pointer, cl, to List_map and this pointer is passed
+ * along to apply as its second argument.
+ *
+ * @param {T} list   List to map with apply
+ * @param {function} apply   Funtion that takes a pointer to a pointer and an
+ *                   		 application-specific pointer
+ * @param {void *} cl   Application-specific pointer to pass to apply as second param	
+ */
+void List_map(T list, void apply(void **x, void *cl), void *cl) {
+	assert(apply);
+	for ( ; list; list = list->rest)
+		apply(&list->first, cl);
+}
+
+/**
+ * Removes the first node in a nonempty list and returns the new list, or returns
+ * an empty list. If x is nonnull, *x is assigned the contents of the first field
+ * of the first node
+ * 
+ * @param  {T} list   List to remove first node
+ * @param  x    [description]
+ * @return      [description]
+ */
+T List_pop (T list, void **x) {
+	if (list) {
+		T head = list->rest;
+		if (x)
+			*x = list->first;
+		FREE(list);
+		return head;
+	} else
+		return list;
+
+}
+
+/**
+ * Adds a new node that holds x to the beginning of list, and returns the new list
+ * 
+ * @param  list List pointer to push x
+ * @param  x    Value to push into list
+ * @return      New list that holds x at the beginning
+ */
+T List_push (T list, void *x) {
+	T p;
+
+	NEW(p);
+	p->first = x;
+	p->rest = list;
+	return p;
+}
+
+/**
+ * Reverses the order of the nodes in its list argument and returns the resulting list
+ * @param  {T} list   List to be reversed
+ * @return     Reversed list
+ */
+T List_reverse (T list) {
+	T head = NULL, next;
+
+	for ( ; list; list = next) {
+		next = list->rest;
+		list->rest = head;
+		head = list;
+	}
+
+	return head;
+}
+
+/**
+ * Creates an array in which elements zero through N-1 hold the N values from the first
+ * field of the list and the Nth element holds the value of end, which is often the null
+ * pointer
+ * 
+ * @param  {T} list   List to create array from
+ * @param  {void * end   Pointer to the value of end
+ * @return      Pointer to the first element of the array created
+ */
+void **List_toArray(T list, void *end) {
+	int i, n = List_length(list);
+	void **array = ALLOC((n + 1) * sizeof(*array));
+
+	for (i = 0; i < n; i++) {
+		array[i] = list->first;
+		list = list->rest;
+	}
+	array[i] = end;
+	return array;
+}
